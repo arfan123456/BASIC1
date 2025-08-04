a2eb10fa88a7a73633fd76b3604e5075a77a0cfd
arfan123456 <128175148+arfan123456@users.noreply.github.com>
2025-08-04 21:28:36 +0600

    Add files via upload

---
+++ package2.py
@@ -0,0 +1,75 @@
+import sys
+import time
+import os
+os.system("clear")
+print("\n\n\n\n\n")
+a="\t\tSystem loading..."
+for c in a:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.1)
+time.sleep(2)
+os.system("clear")
+print("\n\n\n\n\n")
+a="\t\t\033[1;32mSuccesfully loaded!!!!"
+for c in a:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.1)
+n=input("\n\t\t\033[1;35mYour Name: ")
+a="\t\t\033[1;33mHey!\033[1;32m"+n+", \033[1;33mBe Ethical..."
+for c in a:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.1)
+print("\n\n")
+time.sleep(2)
+os.system("clear")
+print("""\033[1;36m
+
+\t\t_____________  _________ 
+\t\t__  ___/__   |/  /__    |
+\t\t_____ \__  /|_/ /__  /| |
+\t\t____/ /_  /  / / _  ___ |
+\t\t/____/ /_/  /_/  /_/  |_|
+                         
+\033[1;33m
+\t=========================================
+\033[1;33m
+\t\tOwner : SMA
+\t\tGithub : Something
+\t\tFacebook: SMA
+\t\tUse only for educational puposes
+\033[1;33m
+\t=========================================
+""")
+print("\033[1;32m")
+os.system("pkg update")
+print("\033[1;33m")
+os.system("pkg upgrade")
+print("\033[1;31m")
+os.system("pkg install python")
+print("\033[1;34m")
+os.system("pip install requests")
+print("\033[1;35m")
+os.system("pip install mechanize")
+print("\033[1;32m")
+os.system("pip install bs4")
+print("\033[1;33m")
+os.system("pkg install git")
+print("\033[1;31m")
+os.system("pip install rich")
+print("\033[1;34m")
+os.system("pkg install espeak")
+print("\033[1;35m")
+os.system("pip install pycurl")
+print("\033[1;36m")
+os.system("pip install httpx")
+print("\033[1;32m")
+os.system("termux-setup-storage")
+print("\033[1;33m")
+os.system("pkg install ruby")
+print("\033[1;34m")
+os.system("gem install lolcat")
+print("\033[1;34m")
+print("All Package Installed Successfully!")
